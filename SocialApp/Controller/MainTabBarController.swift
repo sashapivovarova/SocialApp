6436ba086def5d0e5303a78b9d8db21b98851a8a
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-01-24 23:27:47 +0900

    MainTabBarController の作成

---
+++ SocialApp/Controller/MainTabBarController.swift
@@ -0,0 +1,15 @@
+//
+//  MainTabBarController.swift
+//  SocialApp
+//
+//  Created by Anastasia Grey on 2024/01/24.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
