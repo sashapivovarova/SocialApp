302bde8c4a751a30c19adf1ed4129dd247d1bdb9
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-01-27 22:04:07 +0900

    viewController の追加

--- SocialApp/Controller/ImageSelectorController.swift
+++ SocialApp/Controller/ImageSelectorController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ImageSelectionController: UIViewController {
+class ImageSelectorController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
--- SocialApp/Controller/MainTabBarController.swift
+++ SocialApp/Controller/MainTabBarController.swift
@@ -9,9 +9,23 @@ import UIKit
 
 class MainTabBarController: UITabBarController {
     
+    //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemPink
+        configureController()
     }
     
+    //MARK: - Helpers
+    func configureController() {
+        view.backgroundColor = .white
+        
+        let feed = FeedController()
+        let notification = NotificationController()
+        let imageSelector = ImageSelectorController()
+        let search = SearchController()
+        let profile = ProfileController()
+        
+        viewControllers = [feed, notification, imageSelector, search, profile]
+    }
 }
