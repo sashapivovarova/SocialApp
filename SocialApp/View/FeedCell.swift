7c89da6d106ae0b8fecb7eb963827a8edb7ffeaf
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-01-29 21:31:06 +0900

    feedCell の修正

--- SocialApp/View/FeedCell.swift
+++ SocialApp/View/FeedCell.swift
@@ -9,14 +9,31 @@ import UIKit
 
 class FeedCell: UICollectionViewCell {
     
+    //MARK: - Properties
+    
+    private let profileImageView: UIImageView = {
+        let pf = UIImageView()
+        pf.contentMode = .scaleAspectFill
+        pf.clipsToBounds = true
+        pf.isUserInteractionEnabled = true
+        // pf.backgroundColor = .black
+        pf.image = #imageLiteral(resourceName: "boston-feat")
+        return pf
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .red
+        backgroundColor = .white
+        
+        addSubview(profileImageView)
+        profileImageView.setDimension(height: 40, width: 40)
+        profileImageView.layer.cornerRadius = 40 / 2
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    //MARK: - helpers
     
 }
