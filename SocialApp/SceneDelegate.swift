642933756c11badef8d4de599f004e20410f628f
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-02-01 21:30:11 +0900

    rootViewController の変更

--- SocialApp/SceneDelegate.swift
+++ SocialApp/SceneDelegate.swift
@@ -18,7 +18,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-        window?.rootViewController = MainTabBarController()
+//        window?.rootViewController = MainTabBarController()
+        window?.rootViewController = UINavigationController(rootViewController: LoginViewController())
         window?.makeKeyAndVisible()
     }
 
