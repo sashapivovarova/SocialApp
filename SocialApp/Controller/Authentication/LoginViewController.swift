a5a05e2e5e27170c40fb4c9e8a4d774062ca4675
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-02-03 17:47:20 +0900

    loginViewController の修正

--- SocialApp/Controller/Authentication/LoginViewController.swift
+++ SocialApp/Controller/Authentication/LoginViewController.swift
@@ -19,12 +19,37 @@ class LoginViewController: UIViewController {
     
     private var emailTextField: UITextField = {
        let et = UITextField()
-        et.textColor = .black
+        et.borderStyle = .none
+        et.textColor = .white
         et.keyboardAppearance = .dark
         et.keyboardType = .emailAddress
+        et.attributedPlaceholder = NSAttributedString(string: "  Email", attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.8)])
         et.backgroundColor = UIColor(white: 1, alpha: 0.1)
         et.setHeigth(50)
         return et
+    }()
+    
+    private let passwordTextField: UITextField = {
+       let pt = UITextField()
+        pt.borderStyle = .none
+        pt.textColor = .white
+        pt.keyboardAppearance = .dark
+        pt.isSecureTextEntry = true
+        pt.attributedPlaceholder = NSAttributedString(string: "  Password", attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.8)])
+        pt.backgroundColor = UIColor(white: 1, alpha: 0.1)
+        pt.setHeigth(50)
+        return pt
+    }()
+    
+    private let loginButton: UIButton = {
+        let bt = UIButton(type: .system)
+        bt.layer.cornerRadius = 10
+        bt.setTitle("Log In", for: .normal)
+        bt.setTitleColor(.white, for: .normal)
+        bt.backgroundColor = #colorLiteral(red: 0.7920432687, green: 0.22240448, blue: 0.6546353698, alpha: 1)
+        bt.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        bt.setHeigth(50)
+        return bt
     }()
     
     //MARK: - Life cycle
@@ -38,12 +63,24 @@ class LoginViewController: UIViewController {
     
     //MARK: - Heplers
     func configureUI() {
-        view.backgroundColor = .black
-        
         navigationController?.navigationBar.barStyle = .black
+        
+        let gradient = CAGradientLayer()
+        gradient.colors = [UIColor.systemBlue.cgColor, UIColor.systemPink.cgColor]
+        gradient.locations = [0.1]
+        view.layer.addSublayer(gradient)
+        gradient.frame = view.frame
+        
         view.addSubview(mainIcon)
         mainIcon.centerX(inview: view)
         mainIcon.setDimension(height: 60, width: 80)
         mainIcon.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 30)
+        
+        let stackView = UIStackView(arrangedSubviews: [emailTextField, passwordTextField, loginButton])
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        
+        view.addSubview(stackView)
+        stackView.anchor(top: mainIcon.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 30, paddingLeft: 30, paddingRight: 30)
     }
 }
