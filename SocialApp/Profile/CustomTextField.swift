fe540dbd8cc175d31c40206677dda1318008a7de
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-02-03 21:56:02 +0900

    CustomTextField Profile を追加

---
+++ SocialApp/Profile/CustomTextField.swift
@@ -0,0 +1,31 @@
+//
+//  CustomTextField.swift
+//  SocialApp
+//
+//  Created by Anastasia Grey on 2024/02/03.
+//
+
+import UIKit
+
+class CustomTextField: UITextField {
+    init(placeHolder: String) {
+        super.init(frame: .zero)
+        
+        let space = UIView()
+        space.setDimension(height: 50, width: 13)
+        leftView = space
+        leftViewMode = .always
+        
+        borderStyle = .none
+        textColor = .white
+        keyboardAppearance = .dark
+        keyboardType = .emailAddress
+        backgroundColor = UIColor(white: 1, alpha: 0.1)
+        setHeigth(50)
+        attributedPlaceholder = NSAttributedString(string: placeHolder, attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
