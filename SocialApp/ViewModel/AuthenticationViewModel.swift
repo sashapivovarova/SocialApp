720beb763255fe39d0209ad596452f4ff656080e
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-02-08 21:57:20 +0900

    ResetPasswordViewModel の作成

--- SocialApp/ViewModel/AuthenticationViewModel.swift
+++ SocialApp/ViewModel/AuthenticationViewModel.swift
@@ -52,3 +52,17 @@ struct SignUpViewModel: AuthenticationViewModel {
         return formIsVaild ? .white : UIColor(white: 1, alpha: 0.7)
     }
 }
+
+struct ResetPasswordViewModel: AuthenticationViewModel {
+    var email: String?
+    
+    var formIsVaild: Bool { return email?.isEmpty == false}
+    
+    var buttonBackgroundColor: UIColor {
+        return formIsVaild ? #colorLiteral(red: 0.7920432687, green: 0.22240448, blue: 0.6546353698, alpha: 1) : #colorLiteral(red: 0.9177675843, green: 0.6426904798, blue: 0.9194675684, alpha: 1).withAlphaComponent(0.5)
+    }
+    
+    var buttonTitleColor: UIColor {
+        return formIsVaild ? .white : UIColor(white: 1, alpha: 0.7)
+    }
+}
