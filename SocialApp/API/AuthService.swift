4c7756e93eb30e5b9c53a955572307613317c2fe
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-02-09 21:21:12 +0900

    AuthCredentials の作成

---
+++ SocialApp/API/AuthService.swift
@@ -0,0 +1,21 @@
+//
+//  AuthService.swift
+//  SocialApp
+//
+//  Created by Anastasia Grey on 2024/02/08.
+//
+
+import UIKit
+import Firebase
+
+struct AuthCredentials {
+    let email: String
+    let password: String
+    let fullName: String
+    let userName: String
+    let profileImage: UIImage
+}
+
+struct AuthService {
+    
+}
