7ab87ba685eed707ab579e3c706ea3be8e2ee7b2
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-02-10 21:19:34 +0900

    AuthService の追加

--- SocialApp/API/AuthService.swift
+++ SocialApp/API/AuthService.swift
@@ -17,5 +17,24 @@ struct AuthCredentials {
 }
 
 struct AuthService {
-    
+    static func signUpUser(withCredential credentials: AuthCredentials, completion: @escaping(Error?) -> Void) {
+        ImageUploader.uploadImage(image: credentials.profileImage) { imageUrl in
+            Auth.auth().createUser(withEmail: credentials.email, password: credentials.password) { result, error in
+                if let error = error {
+                    print("DEBUG: Failed to SignUp \(error.localizedDescription)")
+                    return
+                }
+                
+                guard let uid = result?.user.uid else { return }
+                
+                var data: [String: Any] = ["email": credentials.email,
+                                           "fullName": credentials.fullName,
+                                           "profileImage": credentials.profileImage,
+                                           "uid": uid,
+                                           "userName": credentials.userName]
+                
+                Firestore.firestore().collection("Users").document(uid).setData(data, completion: completion)
+            }
+        }
+    }
 }
