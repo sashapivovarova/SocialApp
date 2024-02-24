b3d8f2693f2d485c2b25bf2bc89fe3f7c1103e90
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-02-24 19:56:22 +0900

    profileController の修正

--- SocialApp/Controller/ProfileController.swift
+++ SocialApp/Controller/ProfileController.swift
@@ -7,12 +7,45 @@
 
 import UIKit
 
-class ProfileController: UIViewController {
+private let cellIdentifier = "ProfileCell"
+private let headerIdentifire = "ProfileHeader"
+
+class ProfileController: UICollectionViewController {
     
+    //MARK: - Properties
+    
+    //MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .brown
+        configureUI()
     }
     
+    func configureUI() {
+        collectionView.backgroundColor = .white
+        collectionView.register(ProfileCell.self, forCellWithReuseIdentifier: cellIdentifier)
+        collectionView.register(ProfileHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: headerIdentifire)
+    }
+    //MARK: - Actions
+    
+    //MARK: - Helpers
 }
+
+//MARK: - UICollectionViewDataSource
+extension ProfileController {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath) as! ProfileCell
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerIdentifire, for: indexPath) as! ProfileHeader
+        return header
+    }
+}
+
+
