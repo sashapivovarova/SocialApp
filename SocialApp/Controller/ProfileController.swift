656b914ae8c1c106f44b4230be5710e3b67b4925
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-02-25 10:24:33 +0900

    profileController の修正2

--- SocialApp/Controller/ProfileController.swift
+++ SocialApp/Controller/ProfileController.swift
@@ -48,4 +48,22 @@ extension ProfileController {
     }
 }
 
-
+//MARK: - UICollectionViewDelegateLayOut
+extension ProfileController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (view.frame.width - 2) / 3
+        return CGSize(width: width, height: width)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 220)
+    }
+}
